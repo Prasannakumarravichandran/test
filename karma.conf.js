caceb25a6b1752e1b9fd928e3511e3c5f428bd8d
Prasannakumarravichandran <120444193+Prasannakumarravichandran@users.noreply.github.com>
2023-04-21 10:10:44 +0530

    karma updated

--- karma.conf.js
+++ karma.conf.js
@@ -10,9 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-coverage-istanbul-reporter'),
-      require('korma-sonarqube-unit-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
+      // require('karma-coverage-istanbul-reporter'),
+      // require('korma-sonarqube-unit-reporter')
 
     ],
     
